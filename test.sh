589ad89e2a5507408b2c22d7641adbfbf9222f8b
Andres <sitosanzdev@gmail.com>
2022-11-06 23:22:44 +0100

    V1.55

--- test.sh
+++ test.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Crear usuario 
-echo -n "Nombre: "
-read nombre
-echo -n "Primer Apellido: "
-read ape1
-echo -n "Segundo Apellido: "
-read ape2
-echo -n "DNI: "
-read dni
+#echo -n "Nombre: "
+#read nombre
+#echo -n "Primer Apellido: "
+#read ape1
+#echo -n "Segundo Apellido: "
+#read ape2
+#echo -n "DNI: "
+#read dni
 
-nombrecortado=$(echo "$nombre" | tr '[:upper:]' '[:lower:]' | cut -b 1)
-ape1cortado=$(echo "$ape1" | tr '[:upper:]' '[:lower:]' | cut -b 1-3)
-ape2cortado=$(echo "$ape2" | tr '[:upper:]' '[:lower:]' | cut -b 1-3)
-dnicortado=$(echo "$dni" | cut -b 6-8)
+#nombrecortado=$(echo "$nombre" | tr '[:upper:]' '[:lower:]' | cut -b 1)
+#ape1cortado=$(echo "$ape1" | tr '[:upper:]' '[:lower:]' | cut -b 1-3)
+#ape2cortado=$(echo "$ape2" | tr '[:upper:]' '[:lower:]' | cut -b 1-3)
+#dnicortado=$(echo "$dni" | cut -b 6-8)
 
-usergen=$nombrecortado$ape1cortado$ape2cortado$dnicortado
+#usergen=$nombrecortado$ape1cortado$ape2cortado$dnicortado
 
-echo "Tu usuario generado es: $usergen"
+#echo "Tu usuario generado es: $usergen"
 
-echo "$nombre:$ape1:$ape2:$dni:$usergen" >> usuarios.csv
+#echo "$nombre:$ape1:$ape2:$dni:$usergen" >> usuarios.csv
 
 
 # Al crear el archivo  usuarios.csv, hacer antes un echo que diga Nombre:Apellido1:Apellido2:DNI 
@@ -49,3 +49,5 @@ echo "$nombre:$ape1:$ape2:$dni:$usergen" >> usuarios.csv
 # echo
 # Run Command
 # echo $user
+
+# ls -l github.sh >> prueba.log | echo "------------$(date +%d%m%Y:%H:%M)------------" >>  prueba.log
