df41fc1e4d3459ac23e62e2087725d0b9da116fe
Andres <sitosanzdev@gmail.com>
2022-11-06 16:30:51 +0100

    V1.4

--- T1A3_SanchezVera_Andres.sh
+++ T1A3_SanchezVera_Andres.sh
@@ -3,6 +3,8 @@
 # Andrés Sánchez
 
 # Exit code 0: Todo ha funcionado correctamente.
+
+# Comprobar si el fichero existe
 if [ -f usuarios.csv ]; then
     echo "El fichero existe"
 else
@@ -11,23 +13,15 @@ else
     echo "Nombre:1erApellido:2oApellido:DNI:NombreUser" >>usuarios.csv
 fi
 
-# Copia seguridad
-
-if [ "copia_usuarios_06112022_15:07:54.zip" -ot "copia_usuarios_06112022_15:09:06.zip" ]; then
-    echo "Copia 1 es mayor"
-
-else
-    echo "Copia 2 es mayor"
-fi
-
-busqueda=$(find . -name "*zip" | wc -l)
-if [[ $buesqueda > "2"]]; then
-    echo "Hay mas de 2"
-fi
-
 # Se crea la copia de seguridad de la carpeta donde se encuentra el script
-#zip -r copia_usuarios_$(date +%d%m%Y)_$(date +%H:%M:%S).zip .
+zip -r copia_usuarios_$(date +%d%m%Y)_$(date +%H:%M:%S).zip .
 
-# Se borra el mas antiguo, buscando archivos zip
-#antugio=$(find . -name "*zip" | sort | head -n 1)
-#rm $antugio
+# Se hace una busqueda para que solo hayan 2 copias de seguridad
+busqueda=$(find . -name "*zip" | wc -l)
+if [[ $busqueda > "2" ]]; then
+    # Se borra el mas antiguo, buscando archivos zip
+    antugio=$(find . -name "*zip" | sort | head -n 1)
+    rm $antugio
+fi
+
+
